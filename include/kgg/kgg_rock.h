99b2dac55201ea132d4cc4628453e399bde4209b
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-03-14 21:33:07 +0000

    added speed parameter to rock class

--- include/kgg/kgg_rock.h
+++ include/kgg/kgg_rock.h
@@ -1,1 +1,27 @@
+#ifndef KGG_ROCK_H
+#define KGG_ROCK_H
 
+#include "bn_fixed.h"
+#include "bn_sprite_ptr.h"
+
+namespace kgg {
+
+class rock
+{
+public:
+    rock(bn::fixed x, bn::fixed y, bn::fixed speed);
+
+    void update();
+
+    bool active() const;
+    bool collides_with(bn::fixed player_x, bn::fixed player_y) const;
+
+private:
+    bn::sprite_ptr _sprite;
+    bn::fixed _speed; // speed
+    bool _active = true;
+};
+
+}
+
+#endif
